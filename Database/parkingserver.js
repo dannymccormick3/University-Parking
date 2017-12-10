a232034d86c5e02e02abf0dd6271757d340a851c
Ben Nichols <benjamin.r.nichols@vanderbilt.edu>
2017-12-10 13:13:16 -0600

    added a function to make reservations

--- Database/parkingserver.js
+++ Database/parkingserver.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const moment = require('moment');
 const connection = mysql.createConnection({
 	host:'parkingdatabase.c9dcrnrodp1p.us-east-2.rds.amazonaws.com',
  	user: 'ParkTeam',
@@ -11,9 +12,32 @@ const LIMIT = 100;
 connection.connect((err)=> {
 })
 app.get('/makeReservation', (req, res) => {
-	var startquery = ""
+	var toIns = {}
+	toIns.UserID = req.query.UserID;
+	toIns.Lot = req.query.Lot;
+	toIns.Space = req.query.Space;
+	if(req.query.ScheduleStart) {
+		toIns.ScheduleStart = req.query.ScheduleStart;
+		toIns.ScheduleEnd = req.query.ScheduleEnd;
+	} else {
+		toIns.ScheduleStart = moment().format('YYYY-MM-DD HH:MM:SS');
+		toIns.ScheduleEnd = moment().add(30, 'minutes').format('YYYY-MM-DD HH:MM:SS');
+	}
+	toIns.Completion = null;
+	var startquery = "INSERT INTO Reservations SET ?";
+	connection.query(startquery, toIns, (err, sqlres) => {
+		if(err){ throw err;}
+		else{
+			var obj = {};
+			obj["ScheduleEnd"] = sqlres.ScheduleEnd;
+			obj["Lot"] = sqlres.Lot;
+			obj["Space"] = sqlres.Space;
+			res.send(JSON.stringify(obj));
+		}
 
-})
+	})
+
+});
 
 app.get('/getAvailableLots', (req, res) => {
 	var startquery = "SELECT Lot, Count(Space) as Numspaces "
